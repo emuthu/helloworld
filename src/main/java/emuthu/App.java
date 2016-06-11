38428572e43ef43d8ec6079c4b19edf24d19afd4
Elango Muthu <emuthu@vmware.com>
2016-06-11 11:25:50 +0530

    Add method sayHello

--- src/main/java/emuthu/App.java
+++ src/main/java/emuthu/App.java
@@ -10,6 +10,10 @@ public class App
 		// TODO Auto-generated constructor stub
 	}
 	
+	public void sayHello() {
+		System.out.println("Hello World");
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
